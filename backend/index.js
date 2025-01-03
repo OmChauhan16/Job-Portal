814339e6ae9b0e7602499d8c06ddaa10743858a2
Om Chauhan <omc5246@gmail.com>
2025-01-04 01:08:46 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173','https://job-portalbackend-ol6g.onrender.com',
+    origin:['http://localhost:5173','https://job-portalbackend-ol6g.onrender.com'],
     credentials:true
 }
 

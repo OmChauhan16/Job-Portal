10287575578436564ffc53e145dd8ff6cb5d42e2
Om Chauhan <omc5246@gmail.com>
2025-01-04 23:51:17 +0530

    Update user.controller.js

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -92,7 +92,7 @@ export const login = async (req, res) => {
             profile: user.profile
         }
 
-        return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpOnly: true, secure:true sameSite: 'lax' }).json({
+        return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpOnly: true, secure:true, sameSite: 'lax' }).json({
             message: `Welcome back ${user.fullname}`,
             user,
             success: true
